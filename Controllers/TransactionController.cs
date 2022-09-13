793cf3947c5159e4d9b5de6d0ad827c43ece0c42
sunmoon843 <alaminsun@yahoo.com>
2022-09-13 17:17:17 +0600

    commit4

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,108 @@
+﻿using Expense_Tracker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expense_Tracker.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: TransactionController
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Transactions.Include(t => t.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: TransactionController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TransactionController/Create
+        public IActionResult AddOrEdit(int id)
+        {
+            PopulateCategories();
+            if (id == 0)
+            {
+                return View(new Transaction());
+            }
+            else
+            {
+                return View(_context.Transactions.Find(id));
+            }
+            
+        }
+
+        // POST: TransactionController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId, CategoryId, Amount, Note, Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if (transaction.TransactionId == 0)
+                {
+                    
+                    _context.Add(transaction);
+                }
+                else
+                {
+                    _context.Update(transaction);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            //ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
+            PopulateCategories();
+            return View(transaction);
+        }
+
+        // GET: TransactionController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TransactionController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transactions == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Context' is null.");
+            }
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [NonAction]
+        public void PopulateCategories()
+        {
+            var CategoryCollection = _context.Categories.ToList();
+            Category defaultCategory = new Category() { CategoryId = 0, Title = "Choose a Category" };
+            CategoryCollection.Insert(0, defaultCategory);
+            ViewBag.Categories = CategoryCollection;
+        }
+    }
+}
